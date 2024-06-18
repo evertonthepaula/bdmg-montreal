16947beacd3f34001e9facefeef808461b97679a
Everton de Paula <evertothepaula@gmail.com>
2024-06-18 16:59:15 -0300

    formulario inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,75 @@
-import { Component, Input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
 import { HelloComponent } from './hello.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AppService, ViaCepResponse } from './app.service';
+import { map, Observable } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HelloComponent, MatExpansionModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HelloComponent,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  viaCepObservable$!: Observable<ViaCepResponse>;
+  
   title = 'BDMG Teste Frontend';
   panelOpenState: boolean = true;
+  viaCepData!: ViaCepResponse;
+  viaCepForm!: FormGroup;
+  
   @Input() name!: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private appService: AppService
+  ) { }
+
+  ngOnInit(): void {
+    this.buildForm(new ViaCepResponse());
+
+    this.viaCepObservable$ = this.appService.get().pipe(map(data => {
+      this.viaCepForm.patchValue(data);
+      return data
+    }))
+  }
+
+  private buildForm({ cep, logradouro, complemento, bairro, localidade, uf, gia, ddd, siafi, ibge }: ViaCepResponse): void {
+    this.viaCepForm = this.formBuilder.group({
+      cep: [cep, Validators.required],
+      logradouro: [logradouro, Validators.required],
+      complemento: [complemento, Validators.required],
+      bairro: [bairro, Validators.required],
+      localidade: [localidade, Validators.required],
+      uf: [uf, Validators.required],
+      gia: [gia, Validators.required],
+      ddd: [ddd, Validators.required],
+      siafi: [{ value: siafi, disabled: true }, Validators.required],
+      ibge: [{ value: ibge, disabled: true }, Validators.required],
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.viaCepForm.valid) {
+      return;
+    }
+  }
+
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable()
+export class ViaCepResponse {
+  cep: string = '';
+  logradouro: string = '';
+  complemento: string = '';
+  bairro: string = '';
+  localidade: string = '';
+  uf: string = '';
+  ibge: string = '';
+  gia: string = '';
+  ddd: string = '';
+  siafi: string = '';
+}
+
+@Injectable({
+  providedIn: 'root'
+})
 export class AppService {
-  constructor() {}
+  constructor(public http: HttpClient) { }
+
+  get(): Observable<ViaCepResponse> {
+    return this.http.get<ViaCepResponse>(`https://viacep.com.br/ws/30160907/json/`);
+  }
 }
